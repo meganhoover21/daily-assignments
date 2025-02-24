b2eaac28a4c80ecf943856cf0a262de52fc49b9a
Megan Hoover <meganhoover21@gmail.com>
2025-02-24 12:54:53 -0700

    initial commit

--- R/day-08.R
+++ R/day-08.R
@@ -18,55 +18,21 @@ df=data.frame(region=state.region, #create new data frame using r objects
 #join your new data.frame to the raw COVID data.  
 #join df to the covid d.f. using the state column as the key. This will add region information to each row of the dataset.
 
-covid_w_region <- merge(covid, df, by = "state", all.x = TRUE)
-
-#compute the daily counts and cumulative totals for both cases and deaths.
-#We will do this for each region.
-library(dplyr)
-# Arrange data by state and date
-covid_w_region <- covid_w_region %>%
-  arrange(state, date)
-
-covid_w_region <- covid_w_region %>%
-  group_by(region, state) %>%
-  
-  mutate(                                         #add new columns w/ calculations
-    daily_cases = cases - lag(cases, default = 0), #This gives the value of cases from the previous row (the previous day). If it's the first row (the first date), the default = 0 argument means that it will return 0 because there is no previous row.
-    daily_deaths = deaths - lag(deaths, default = 0),
-    cumulative_cases = cumsum(daily_cases),
-    cumulative_deaths = cumsum(daily_deaths)
-  ) %>%
-  ungroup()
-
-
-#To make the data suitable for plotting with ggplot2, we need to reshape it from a wide format (with separate columns for cases, deaths, etc.) to a long format.
-library(tidyr)
-
-# Pivot data into long format
-
-covid_deaths_data <- covid_w_region %>%
-  select(date, state, region, deaths, cumulative_cases)
-
-# Step 2: Pivot the data to long format
-covid_deaths_long <- covid_deaths_data %>%
-  pivot_longer(cols = c(deaths, cumulative_cases), 
-               names_to = "metric", 
-               values_to = "count")
+covid_w_region <- inner_join(df, covid, by= "state")%>% #innerjoin (df1, df2, join by=)
+group_by(region,date)%>% #group by region for each date
+summarize(cases = sum(cases),
+          deaths = sum(deaths)) %>% #compute the daily counts and cumulative totals for both cases and deaths.
+pivot_longer(cols= c(cases, deaths),
+             names_to= "type",
+             values_to = "count") 
 
 
 
-#The facet_wrap() function allows us to create a separate plot for each region.
-library(ggplot2)
-
-ggplot(covid_deaths_long, aes(x = date, y = count, color = metric)) +
-  geom_line() +  # Line plot
-  facet_wrap(~ region, scales = "free_y") +  # Facet by region
-  labs(title = "COVID-19 Deaths and Cumulative Deaths by Region",
-       x = "Date",
-       y = "Count",
-       color = "Metric") +
-  theme_minimal() +  # Clean theme
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+covid_w_region %>% #make facet grid when seeing relationship b/w 2 variables.by region and date
+  ggplot(aes(x= date, y= count)) +
+  geom_line() +
+  facet_grid(type~region, scales="free_y") +
+  theme_bw()
 
 ggsave("covid_cases_deaths_by_region.png", width = 12, height = 8, dpi = 300)
   
